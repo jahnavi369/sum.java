f6e928303dcaaa8f38c1db55b2efc5588279e958
jahnavi369 <73998617+jahnavi369@users.noreply.github.com>
2021-09-04 12:14:06 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum  
+{  
+public static void main(String args[])   
+{  
+int n1 = 22, n2 = 11, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+}  
